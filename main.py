1cadf9ef85afe3c421dfb23bde40a49b600c6e2f
Flavio Pace <flavio.pace@gmail.com>
2022-06-05 23:09:36 +0200

    add some improvement

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
 import fastf1.plotting
 import numpy as np
-import pandas as pd
-
 
 fastf1.Cache.enable_cache('/tmp/fastf1')  # replace with your cache directory
 
 # we only want support for timedelta plotting in this example
 fastf1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme=None, misc_mpl_mods=False)
 
-session = fastf1.get_session(2022, 'Spanish Grand Prix', 'Q')
+session = fastf1.get_session(2022, 'Imola', 'Q')
 session.load()
 
 # Get the laps
